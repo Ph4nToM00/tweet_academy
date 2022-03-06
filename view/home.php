f13b2c97021072355a926d264f525dc72ffc156c
Joris <Joris.guillot@epitech.eu>
2022-03-06 16:51:34 +0100

    home twitter

--- view/home.php
+++ view/home.php
@@ -16,7 +16,7 @@
             <div class="sticky">
                 <img src="../images/twitterA.png" alt="">
                 <a href="../index.php?action=home" style="text-decoration: none;"><li>HOME</li></a>
-                <a href="../view/profil.php" style="text-decoration: none;"><li>PROFIL</li></a>
+                <a href="../index.php?action=profil" style="text-decoration: none;"><li>PROFIL</li></a>
                 <a href="../index.php?action=messagerie" style="text-decoration: none;"><li>MESSAGERIE</li></a>
                 <a href="../index.php?action=disconnect" style="text-decoration: none;"><li div="disco">DECONNECTION</li></a>
 
